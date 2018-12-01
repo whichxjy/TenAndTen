293189d72679d8758cd281e40e77a1a3816e102c
VitasoyKiller <531195874@qq.com>
2018-12-02 05:43:17 +0800

    基本 Struts2 框架配置

---
+++ src/hello/MessageStore.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class MessageStore {
+    private String message;
+    
+    public MessageStore() {
+        message = "Hello Struts User";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
